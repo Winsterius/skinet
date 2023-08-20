0dabe24a918ea4353f8e47123d0560f64ceb0c18
jurischreiner <Jura.sch90@gmail.com>
2023-08-20 13:16:26 +0200

    end of section 8

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+import {Product} from "./Models/Product";
+import {Pagination} from "./Models/Pagination";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'client';
+  products: Product[] = [];
+  constructor(private http: HttpClient) {}
+
+  ngOnInit(): void {
+    this.http.get<Pagination<Product[]>>('https://localhost:5001/api/products').subscribe({
+      next: (response) => this.products = response.data,
+      error: error => console.log(error),
+      complete: () => {
+        console.log('request completed');
+      }
+    })
+  }
 }
